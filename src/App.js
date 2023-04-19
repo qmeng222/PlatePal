cb9e4b9d1502d94831fa5f18b11ec38486cf2de7
Qingying Meng <qmeng222@gmail.com>
2023-04-18 23:52:41 -0700

    setup the component tree

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import MainComponent from "./components/MainComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>This is the home page!</h1>
+      <MainComponent />
+    </div>
+  );
 }
 
 export default App;
