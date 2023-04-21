434d15911a52667cd78ada59794fdbadd459d7ae
Qingying Meng <qmeng222@gmail.com>
2023-04-21 00:36:57 -0700

    designed menu item

--- src/components/body/MenuItem.jsx
+++ src/components/body/MenuItem.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 const MenuItem = (props) => {
   console.log(props);
-  return <div></div>;
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.dish.image} />
+        <Card.Body>
+          <Card.Title style={{ cursor: "pointer" }}>
+            {props.dish.name}
+          </Card.Title>
+          <Card.Text>{props.dish.description}</Card.Text>
+          <p>
+            <b>Price: {props.dish.price}</b>
+          </p>
+          <p>
+            <b>Label: {props.dish.label}</b>
+          </p>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default MenuItem;
