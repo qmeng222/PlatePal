8613658bcbc26b18eea24ac1d38b31bbd0e4ec14
Qingying Meng <qmeng222@gmail.com>
2023-04-23 23:06:08 -0700

    added routes

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Home from "./Home";
 import Menu from "./Menu";
+import About from "./About";
+import Contact from "./Contact";
+import { Routes, Route } from "react-router-dom";
 
 const Body = () => {
   return (
     <div>
-      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="contact/" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
