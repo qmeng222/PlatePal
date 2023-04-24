82a626ca6f870eb7a4a9ec3e2ae7ce5c213f32bf
Qingying Meng <qmeng222@gmail.com>
2023-04-23 22:42:30 -0700

    designed Navbar

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -1,31 +1,42 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../../assets/css/Main.css";
+import Logo from "../../assets/image/logo.png";
 
 const Navigation = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">PlatePal</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/">
+              <img src={Logo} alt="logo" />
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Others</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link>
+                <Link to="/" className="mainmenu">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/menu" className="mainmenu">
+                  Menu
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about" className="mainmenu">
+                  About
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/contact" className="mainmenu">
+                  Contact
+                </Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
