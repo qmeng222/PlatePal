7f0987d6b831a1a18e7708b41107f3f17d6fd497
Qingying Meng <qmeng222@gmail.com>
2023-04-24 19:58:40 -0700

    handled the Contact form

--- src/components/body/Contact.jsx
+++ src/components/body/Contact.jsx
@@ -9,34 +9,89 @@ import {
 } from "react-bootstrap";
 
 export class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      mobile: "",
+      email: "",
+      message: "",
+      agree: false,
+    };
+  }
+
+  handleInputChange = (event) => {
+    // called for all input fields, but only the agree property is updated in the state object:
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    console.log(this.state);
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div>
         <Fragment>
           <Container>
             <h1>Send us your feedback!</h1>
-            <Form>
+
+            <Form onSubmit={this.handleSubmit}>
               <Row className="mb-3">
                 <Form.Group as={Col}>
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="text" placeholder="First Name" />
+                  <Form.Control
+                    type="text"
+                    name="firstname"
+                    placeholder="First Name"
+                    value={this.state.firstname}
+                    onChange={this.handleInputChange}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col}>
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="text" placeholder="Last Name" />
+                  <Form.Control
+                    type="text"
+                    name="lastname"
+                    placeholder="Last Name"
+                    value={this.state.lastname}
+                    onChange={this.handleInputChange}
+                  />
                 </Form.Group>
               </Row>
 
               <Row className="mb-3">
                 <Form.Group as={Col}>
                   <Form.Label>Mobile</Form.Label>
-                  <Form.Control type="number" placeholder="Mobile" />
+                  <Form.Control
+                    type="number"
+                    name="mobile"
+                    placeholder="Mobile"
+                    value={this.state.mobile}
+                    onChange={this.handleInputChange}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col}>
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter your email" />
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                  />
                 </Form.Group>
               </Row>
 
@@ -46,11 +101,24 @@ export class Contact extends Component {
                 label="Leave a message here"
                 className="mb-3"
               >
-                <Form.Control as="textarea" style={{ height: "200px" }} />
+                <Form.Control
+                  as="textarea"
+                  // placeholder="Leave a comment here"
+                  name="message"
+                  value={this.state.message}
+                  onChange={this.handleInputChange}
+                  style={{ height: "200px" }}
+                />
               </FloatingLabel>
 
               <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="May we contact you?" />
+                <Form.Check
+                  type="checkbox"
+                  label="May we contact you?"
+                  name="agree"
+                  checked={this.state.agree}
+                  onChange={this.handleInputChange}
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit">
