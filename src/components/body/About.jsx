68fd7720b389b8fba3e515a12162910564e01ad8
Qingying Meng <qmeng222@gmail.com>
2023-04-24 20:45:55 -0700

    worked on Footer component

--- src/components/body/About.jsx
+++ src/components/body/About.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <Fragment>
+        <Container>
+          <h1>About Page</h1>
+
+          <p>
+            <b>What is Lorem Ipsum?</b>
+            <br />
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <b>Why do we use it?</b>
+          <br />
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </Container>
+      </Fragment>
     </div>
   );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "../../assets/css/Main.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h3>Footer</h3>
+    <div className="footer">
+      <div className="container">
+        {/* create a horizontal row of elements, and horizontally center the content: */}
+        <div className="row justify-content-center">
+          {/* make the width of the column automatically adjust to the content it contains: */}
+          <div className="col-auto">
+            <p>&copy; 2023 PlatePal. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
